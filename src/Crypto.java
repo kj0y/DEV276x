ddd8b7221626d78c0cce5bd184e416bcccbd6e75
Kandis Flick <kandisflick@gmail.com>
2019-04-11 07:55:05 -0700

    add beginning of Crypto project

--- src/Crypto.java
+++ src/Crypto.java
@@ -2,14 +2,34 @@ import java.util.*;
 
 public class Crypto {
     static public void main(String[] args) {
-        normalizeText("This is some \"really\" great. (Text)!?");
+        System.out.println(normalizeText("This is some \"really\" great. (Text)!?"));
+        System.out.println(Ceasarify("abc", 3));
     }
 
-    public static char normalizeText(String s) {
-        char c = 0;
-        for (int i = 0; i < s.length(); i++) {
-            c = s.charAt(i);
-        }
-        return c;
+    // Part 1 - Normalize Text
+    public static String normalizeText(String s) {
+        String normalizedString = s.replaceAll("[^\\p{L}\\p{N}]", "").toUpperCase();
+        return normalizedString;
     }
+
+    // Part 2 - Ceasar Cipher
+    public static String Ceasarify(String message, int key) {
+        String s = "";
+        int len = message.length();
+        for (int x = 0; x < len; x++) {
+            char c = (char) (message.charAt(x) + key);
+            if (c > 'z')
+                s += (char) (message.charAt(x) - (26 - key));
+            else
+                s += (char) (message.charAt(x) + key);
+        }
+        return s;
+    }
+
+    // Part 3 - Codegroups
+    public static String Codegroups(String encryptedMsg, int numLettersPerGrp) {
+        String x = "";
+        return x;
+    }
+
 }
